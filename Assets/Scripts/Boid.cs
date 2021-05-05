197500bdd32618809ea5e33acea57df668f7eb9b
JoJoJet <joe102000@gmail.com>
2021-05-05 14:32:46 -0400

    Cache some property calls

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -16,6 +16,9 @@ public class Boid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var thisPosition = this.transform.position; // Cache these values because the properties are slow to call.
+        var thisForward = this.transform.forward;
+
         var flockSqr = mommy.localFlockRadius * mommy.localFlockRadius;
         var sepSqr = mommy.separationRadius * mommy.separationRadius;
 
@@ -26,19 +29,20 @@ public class Boid : MonoBehaviour
         int flockSize = 0;
         var flock = new List<(float sqrDist, Boid boid)>();
         foreach(var b in mommy.bubbies) {
-            var sqrDist = Vector3.SqrMagnitude(b.transform.position - this.transform.position);
+            var bPosition = b.transform.position;
+            var sqrDist = Vector3.SqrMagnitude(bPosition - thisPosition);
             if(sqrDist > flockSqr) {
                 continue;
             }
             // The boid is within the local flock.
             flockSize++;
 
-            centerOfMass += b.transform.position;
+            centerOfMass += bPosition;
             heading += b.transform.forward;
 
             // The boid is too close, we should try to avoid it.
             if(sqrDist < sepSqr) {
-                separation += this.transform.position - b.transform.position;
+                separation += thisPosition - bPosition;
             }
         }
 
@@ -46,12 +50,12 @@ public class Boid : MonoBehaviour
         centerOfMass /= flockSize;
         heading /= flockSize;
 
-        var toCenterOfMass = (centerOfMass - this.transform.position).normalized;
+        var toCenterOfMass = (centerOfMass - thisPosition).normalized;
         toCenterOfMass *= (mommy.preferredFlockCount - flockSize) / mommy.preferredFlockCount;
 
 
-        var dir = transform.forward + toCenterOfMass + heading + separation;
-        if(Physics.Raycast(new Ray(transform.position, transform.forward.normalized), out var hit, mommy.rayDistance)) {
+        var dir = thisForward + toCenterOfMass + heading + separation;
+        if(Physics.Raycast(new Ray(thisPosition, thisForward), out var hit, mommy.rayDistance)) {
             var norm = hit.normal;
 
             if(Physics.Raycast(hit.point, norm * mommy.rayDistance / 4, out var hit2)) {
@@ -65,11 +69,11 @@ public class Boid : MonoBehaviour
 
         dir = dir.normalized;
 
-        if(!mommy.box.bounds.Contains(transform.position)) {
-            dir = mommy.box.bounds.center - this.transform.position;
+        if(!mommy.box.bounds.Contains(thisPosition)) {
+            dir = mommy.box.bounds.center - thisPosition;
         }
 
-        dir = Vector3.Slerp(transform.forward, dir, 0.1f);
+        dir = Vector3.Slerp(thisForward, dir, 0.1f);
 
         transform.rotation = Quaternion.LookRotation(dir);
         transform.Translate(Vector3.forward * mommy.speed * Time.deltaTime);
