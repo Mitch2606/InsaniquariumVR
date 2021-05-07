4f42da4287da3a2ed0cdd8aba3a02c13d37a837b
JoJoJet <joe102000@gmail.com>
2021-05-07 17:35:31 -0400

    Add headers to Species parameters

--- Assets/Scripts/Species.cs
+++ Assets/Scripts/Species.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Species", menuName = "Boids/Species", order = 1)]
 public class Species : ScriptableObject
 {
+    [Header("Movement speed")]
     public float speed = 10f;
-    public float localFlockRadius = 5f;
-    public float flockFactor = 1f;
+
+    [Header("Rule #1: Separation")]
     public float separationRadius = 1f;
     public float maxSeparationAmt = 3f;
+
+    [Header("Rule #3: Cohesion")]
+    public float localFlockRadius = 5f;
+    public float flockFactor = 1f;
     public float preferredFlockCount = 20;
+    
+    [Header("Obstacle avoidance")]
     public float rayDistance = 2;
     public float obstacleFactor = 1f;
 }
